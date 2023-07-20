74fe5ebdc590efa8f8dc60ec0f93cc8e2bc7e0e8
Karangwa Ajika <52496651+karangwaajika@users.noreply.github.com>
2023-07-20 09:13:52 +0200

    test: create html, css and js files and display output on console

---
+++ main.js
@@ -0,0 +1,17 @@
+const add = (a, b)=>{
+    return a+b;
+};
+const subtract = (a, b)=>{
+    return a-b;
+};
+const multiply = (a, b)=>{
+    return a*b;
+};
+const divide = (a, b)=>{
+    return a/b;
+};
+
+console.log(add(1,2));
+console.log(subtract(1,2));
+console.log(multiply(1,2));
+console.log(divide(1,2));
